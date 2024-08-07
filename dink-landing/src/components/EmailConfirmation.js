a623eb6fd63d85a0d231e6e8534e22ebc142cfac
Jesse Pelletier <jespel2013@gmail.com>
2024-08-07 13:25:55 -0400

    changed snackbar format

--- dink-landing/src/components/EmailConfirmation.js
+++ dink-landing/src/components/EmailConfirmation.js
@@ -1,28 +1,38 @@
 import { Slide, Snackbar, Typography } from "@mui/material";
-import { ColorCode } from "../enum/colorCodes.ts"
+import { ColorCode } from "../enum/colorCodes.ts";
 
 const EmailConfirmation = (props) => {
-    const {isOpen, setIsOpen} = props
+  const { isOpen, setIsOpen } = props;
 
-    const handleClose = () => {
-        setIsOpen(false)
-    }
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
-    const message = (<Typography sx={{fontFamily:'Lato'}}>Thanks for signing up! Keep an eye out for an email when our Beta is available!</Typography>)
-    return (
-        <div>
-
-            <Snackbar
-                anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
-                open={isOpen}
-                onClose={handleClose}
-                message={message}
-                TransitionComponent={Slide}
-                autoHideDuration={7000}
-                ContentProps={{sx: {backgroundColor:ColorCode.StormGreen, borderRadius:'12px'}}}
-            />
-        </div>
-    )
-}
+  const message = (
+    <div>
+      <Typography sx={{ fontFamily: "Lato" }}>
+        Thanks for signing up!
+      </Typography>
+      <Typography sx={{ fontFamily: "Lato" }}>
+        Keep an eye out for an email when our Beta is available!
+      </Typography>{" "}
+    </div>
+  );
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={isOpen}
+        onClose={handleClose}
+        message={message}
+        TransitionComponent={Slide}
+        autoHideDuration={7000}
+        ContentProps={{
+          sx: { backgroundColor: ColorCode.Jade, borderRadius: "12px" },
+        }}
+      />
+    </div>
+  );
+};
 
 export default EmailConfirmation;
