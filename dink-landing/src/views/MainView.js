5a9b1bb9bc3aa28eafe3a4af3b8434969e326995
Jesse Pelletier <jespel2013@gmail.com>
2024-08-24 10:33:52 -0400

    removed non-mobile-compliant features
    1) Removed fade in sections
    2) Removed nav menubar

--- dink-landing/src/views/MainView.js
+++ dink-landing/src/views/MainView.js
@@ -13,47 +13,25 @@ const MainView = () => {
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const toggleConfirmation = () => {
-    showConfirmation ? setShowConfirmation(false) : setShowConfirmation(true)
-  }
+    showConfirmation ? setShowConfirmation(false) : setShowConfirmation(true);
+  };
 
   return (
     <div>
       <VideoLogo />
-      <MenuDrawer />
-      <EmailConfirmation isOpen={showConfirmation} setIsOpen={setShowConfirmation}/>
+      {/* <MenuDrawer /> */}
+      <EmailConfirmation
+        isOpen={showConfirmation}
+        setIsOpen={setShowConfirmation}
+      />
       {/*<MenuBar />*/}
       <Grid
-        sx={{padding: "5%" }}
+        sx={{ padding: "5%" }}
         alignItems="center"
         container
         justifyContent="center"
         spacing={2}
       >
-        <FadeInSection
-          alignment={Alignment.LEFT}
-          message={<Typography>We play pickleball...</Typography>}
-        />
-        <FadeInSection
-          alignment={Alignment.RIGHT}
-          message={<Typography>...but not as much as we'd like to.</Typography>}
-        />
-        <FadeInSection
-          alignment={Alignment.LEFT}
-          message={
-            <Typography>
-              We saw a need for a way to find new partners, new courts, and new
-              ways to play...
-            </Typography>
-          }
-        />
-        <FadeInSection
-          alignment={Alignment.RIGHT}
-          message={
-            <Typography>
-              So we built <img src={dinkLogo} alt="dinkilicious" />
-            </Typography>
-          }
-        />
         <FadeInSection
           alignment={Alignment.CENTER}
           message={
@@ -68,7 +46,7 @@ const MainView = () => {
                 ></TextField>
                 <Button sx={{ backgroundColor: ColorCode.StormGreen, ml:'20px'}} size='large' variant='contained'>Submit!</Button>
               </Box> */}
-              <SignUpSection toggleConfirmation={toggleConfirmation}/>
+              <SignUpSection toggleConfirmation={toggleConfirmation} />
             </>
           }
         />
